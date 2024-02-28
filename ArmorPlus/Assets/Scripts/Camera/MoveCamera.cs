387ef311c585a4ae624faba2ad62cc52cd32f726
noobissk <noobissk@gmail.com>
2024-02-28 23:16:01 +0100

    TA KAMERA FUNGIIIIIIIIIIIIIIIIIRUJE

--- ArmorPlus/Assets/Scripts/Camera/MoveCamera.cs
+++ ArmorPlus/Assets/Scripts/Camera/MoveCamera.cs
@@ -8,35 +8,54 @@ public class MoveCamera : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private float dragSensitivity = 3;
-    [SerializeField] private float drag = 3;
     private Controls controls;
-    private Vector2 difference, origin;
-    private Vector2 cursorPosition;
+    private Vector3 difference, origin;
+    private Vector3 cursorPosition, cameraPosition;
+    private Vector3 originCamPos;
+    private Vector3 cameraOffset;
+    private float camOffset;
     private bool isdragging;
     private void Start()
     {
         controls = UserSettings.controls;
-        origin = cam.transform.position;
+        originCamPos = cam.transform.position;
+        // origin = originCamPos;
     }
 
     void Update()
     {
-
+        Vector3 offset = GetCameraOffset(transform.position.y, cam.transform.eulerAngles.x, cam.transform.eulerAngles.y);
+        // Debug.Log(offset);
+        transform.position = Vector3.Lerp(transform.position, origin - difference + offset, Time.deltaTime * dragSensitivity);
         if (controls.InGame.Fire.IsPressed())
         {
-            cursorPosition = UserSettings.GetInputPositions();
+            Plane plane = new Plane(Vector3.up, new Vector3(cam.transform.position.x, 0, cam.transform.position.z));
+            Ray cursorRay = cam.ScreenPointToRay(UserSettings.GetInputPositions());
+            Ray camRay = new Ray(cam.transform.position, cam.transform.forward);
 
-            difference = cursorPosition - new Vector2(Screen.width, Screen.height)*0.5f;
+
+            plane.Raycast(cursorRay, out float cursorRange);
+            plane.Raycast(camRay, out float camRange);
+
+            cursorPosition = cursorRay.GetPoint(cursorRange);
+            cameraPosition = camRay.GetPoint(camRange);
+
+            difference = cursorPosition - cameraPosition;
             if (!isdragging)
             {
                 isdragging = true;
                 origin = cursorPosition;
             }
+            return;
         }
-        else
-        {
-            isdragging = false;
-        }
-        cam.transform.position = Vector3.Lerp(cam.transform.position, difference * dragSensitivity, Time.deltaTime * 15);
+        isdragging = false;
+            // cam.transform.position = Vector3.Lerp(cam.transform.position, origin - difference * dragSensitivity, Time.deltaTime * 15);
+    }
+
+
+    private Vector3 GetCameraOffset(float height, float xAngle, float yAngle)
+    {
+        float tanEquation = height/Mathf.Tan(xAngle * Mathf.Deg2Rad);
+        return new Vector3(-tanEquation*Mathf.Cos(yAngle * Mathf.Deg2Rad), height, -tanEquation*Mathf.Sin(yAngle * Mathf.Deg2Rad));
     }
 }
