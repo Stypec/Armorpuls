8ed1f2ea807181e7cd9559503fb1b022acf79194
noobissk <noobissk@gmail.com>
2024-06-20 09:46:32 +0200

    store updates based on item list

--- ArmorPlus/Assets/Scripts/Items/StoreItemValues.cs
+++ ArmorPlus/Assets/Scripts/Items/StoreItemValues.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class StoreItemValues : MonoBehaviour
 {
+    [SerializeField] TMP_Text price;
+    [SerializeField] public Image sprite;
     public ItemObject item;
     public int amount;
+    public void Visualize()
+    {
+        price.text = amount.ToString();
+        if (sprite.sprite == null)
+            sprite.color = new Color(0, 0, 0, 0);
+    }
 }
